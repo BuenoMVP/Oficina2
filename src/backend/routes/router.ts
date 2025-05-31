0fd4cf8b0441263010477056c26a6b730eff5018
Marcos Bueno <marcos_vbp@outlook.com>
2025-05-31 09:57:43 -0300

    feat: add Integrantes CRUD and route

--- src/backend/routes/router.ts
+++ src/backend/routes/router.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express"
 import usuariosRouter from "./usuarios"
+import integrantesRouter from "./integrantes"
 
 const router = express.Router()
 
 router.use('/usuarios', usuariosRouter)
+router.use('/integrantes', integrantesRouter)
 
 router.use('/', (_req: Request, res: Response) => {
     res.send('Página inicial da api')
