a17c62c646365fe73f97bda57eec8987e3637a7b
Marcos Bueno <marcos_vbp@outlook.com>
2025-05-30 22:37:33 -0300

    feat: add data base connection

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,6 +3,9 @@ import { server } from "./config/server"
 import dotenv from "dotenv"
 dotenv.config()
 
+import { connectDB } from "./config/connectDB"
+connectDB()
+
 const port = process.env.VITE_PORT || 8081
 
 server.listen(port, () => {
